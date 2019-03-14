2444a693cc5a85509bec30e24796eb3f08023e6a
nontoa <nicolasnc010@gmail.com>
2019-03-14 18:05:45 -0500

    JavaScript

--- src/main/resources/static/JS/apimock.js
+++ src/main/resources/static/JS/apimock.js
@@ -11,12 +11,33 @@ apimock = (function () {
 	mockdata["Cine112"] = [{ "name": "Cine112", "functions": [{ "movie": { "name": "mision imposible", "genre": "Fiction" }, "seats": [[true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true], [true, true, true, true, true, true, true, true, true, true, true, true]], "date": "2018-12-18 15:31" }] }];
 
 
+	var  nameCinema = function(){
+
+		return mockdata.name;
+	};
+
+	var listaFunctions = function(){
+		return mockdata.functions;
+	};
+
+	var setName = function(name){
+		mockdata[name].name=nameCinema;
+		mockdata[name].functions=listaFunctions;
+		return mockdata[name];
+	};
+
+
 	return {
 		getCinemaByName: function (name, callback) {
 			callback(
 				mockdata[name]
+				
 			);
-		}
+		},
+		publicMethod : setName(name)
+
 	}
 
 })();
+
+apimock.getCinemaByName("Cine80");
